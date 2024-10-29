07134545ff03e6b4a0d443846592847d02b820ae
Serguei Kozumine <kouzmine_serguei@yahoo.com>
2024-10-29 12:25:44 -0400

    attempt to  debug

--- test/integration/default/test.rb
+++ test/integration/default/test.rb
@@ -1,7 +1,12 @@
 control 'gcs-bucket' do
     impact 1.0
     title 'Check if the GCS bucket exists'
-    describe google_storage_bucket(name: "#{input('bucket_name')}d") do
+    describe google_storage_bucket(name: "#{input('bucket_name')}") do
       it { should exist }
+      let(:x) {subject}
+      x.instance_variables.each do |var|
+        puts "#{var}: #{allowed_rules[0].instance_variable_get(var)}"
+      end
+ 
     end
   end
